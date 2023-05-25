1c6bee49fdf8ae8dc095f0445af28cf8c308530b
Bruno Mandrick <brunomandrick@alunos.utfpr.edu.br>
2023-05-25 16:47:00 -0300

    r0

--- Prot_Sistemas/Form2.Designer.cs
+++ Prot_Sistemas/Form2.Designer.cs
@@ -44,6 +44,12 @@ namespace Prot_Sistemas
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,63 +57,76 @@ namespace Prot_Sistemas
             // 
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(31, 80);
+            this.comboBox1.Location = new System.Drawing.Point(215, 127);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(102, 21);
+            this.comboBox1.Size = new System.Drawing.Size(89, 24);
             this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // comboBox2
             // 
             this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(31, 107);
+            this.comboBox2.Location = new System.Drawing.Point(215, 161);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(102, 21);
+            this.comboBox2.Size = new System.Drawing.Size(89, 24);
             this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // comboBox3
             // 
             this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(31, 134);
+            this.comboBox3.Location = new System.Drawing.Point(215, 194);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(102, 21);
+            this.comboBox3.Size = new System.Drawing.Size(89, 24);
             this.comboBox3.TabIndex = 1;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
             // 
             // comboBox4
             // 
             this.comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(161, 103);
+            this.comboBox4.Location = new System.Drawing.Point(41, 99);
+            this.comboBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox4.Name = "comboBox4";
-            this.comboBox4.Size = new System.Drawing.Size(102, 21);
+            this.comboBox4.Size = new System.Drawing.Size(75, 24);
             this.comboBox4.TabIndex = 1;
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
             // 
             // comboBox5
             // 
             this.comboBox5.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox5.FormattingEnabled = true;
-            this.comboBox5.Location = new System.Drawing.Point(161, 130);
+            this.comboBox5.Location = new System.Drawing.Point(41, 132);
+            this.comboBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox5.Name = "comboBox5";
-            this.comboBox5.Size = new System.Drawing.Size(102, 21);
+            this.comboBox5.Size = new System.Drawing.Size(75, 24);
             this.comboBox5.TabIndex = 1;
+            this.comboBox5.SelectedIndexChanged += new System.EventHandler(this.comboBox5_SelectedIndexChanged);
             // 
             // comboBox6
             // 
             this.comboBox6.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox6.FormattingEnabled = true;
-            this.comboBox6.Location = new System.Drawing.Point(161, 157);
+            this.comboBox6.Location = new System.Drawing.Point(41, 167);
+            this.comboBox6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox6.Name = "comboBox6";
-            this.comboBox6.Size = new System.Drawing.Size(102, 21);
+            this.comboBox6.Size = new System.Drawing.Size(75, 24);
             this.comboBox6.TabIndex = 1;
+            this.comboBox6.SelectedIndexChanged += new System.EventHandler(this.comboBox6_SelectedIndexChanged);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(381, 240);
+            this.pictureBox1.Size = new System.Drawing.Size(508, 240);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -115,71 +134,143 @@ namespace Prot_Sistemas
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 83);
+            this.label1.Location = new System.Drawing.Point(12, 102);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.Size = new System.Drawing.Size(18, 16);
             this.label1.TabIndex = 2;
             this.label1.Text = "Ia";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 110);
+            this.label2.Location = new System.Drawing.Point(12, 135);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.Size = new System.Drawing.Size(18, 16);
             this.label2.TabIndex = 2;
             this.label2.Text = "Ib";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(9, 138);
+            this.label3.Location = new System.Drawing.Point(12, 170);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(16, 13);
+            this.label3.Size = new System.Drawing.Size(17, 16);
             this.label3.TabIndex = 2;
             this.label3.Text = "Ic";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(139, 106);
+            this.label4.Location = new System.Drawing.Point(185, 130);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.Size = new System.Drawing.Size(24, 16);
             this.label4.TabIndex = 2;
             this.label4.Text = "Va";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(139, 133);
+            this.label5.Location = new System.Drawing.Point(185, 164);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(20, 13);
+            this.label5.Size = new System.Drawing.Size(24, 16);
             this.label5.TabIndex = 2;
             this.label5.Text = "Vb";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(139, 160);
+            this.label6.Location = new System.Drawing.Point(185, 197);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(20, 13);
+            this.label6.Size = new System.Drawing.Size(23, 16);
             this.label6.TabIndex = 2;
             this.label6.Text = "Vc";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(320, 209);
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.Location = new System.Drawing.Point(427, 257);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(49, 20);
+            this.button1.Size = new System.Drawing.Size(65, 25);
             this.button1.TabIndex = 3;
             this.button1.Text = "OK";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(152, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(78, 15);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 31);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 16);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Pri";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 47);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(31, 16);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "Sec";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(152, 48);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(78, 15);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "1";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Location = new System.Drawing.Point(59, 31);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(46, 15);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "1";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Location = new System.Drawing.Point(59, 47);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(46, 15);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.Text = "1";
+            // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(381, 241);
+            this.ClientSize = new System.Drawing.Size(508, 297);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -195,6 +286,7 @@ namespace Prot_Sistemas
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form2";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -221,5 +313,11 @@ namespace Prot_Sistemas
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
     }
 }
--- Prot_Sistemas/Form2.cs
+++ Prot_Sistemas/Form2.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,11 +16,46 @@ namespace Prot_Sistemas
     {
         public Form2()
         {
-            InitializeComponent();            
+            InitializeComponent();
         }        
         private void Form2_Load(object sender, EventArgs e)
         {
             draw_design();
+            for (int x = 0; x < 10; x++)
+            {
+                if (Analogico_A_forms2[x] != null)
+                {
+                    comboBox4.Items.Add(Analogico_A_forms2[x]);
+                    comboBox5.Items.Add(Analogico_A_forms2[x]);
+                    comboBox6.Items.Add(Analogico_A_forms2[x]);
+                    
+                }
+                if (Analogico_V_forms2[x] != null)
+                {
+                    comboBox1.Items.Add(Analogico_V_forms2[x]);
+                    comboBox2.Items.Add(Analogico_V_forms2[x]);
+                    comboBox3.Items.Add(Analogico_V_forms2[x]);
+                    
+                }
+            }
+            comboBox4.SelectedItem = comboBox4.Items[0];
+            comboBox5.SelectedItem = comboBox5.Items[1];
+            comboBox6.SelectedItem = comboBox6.Items[2];
+            comboBox1.SelectedItem = comboBox1.Items[0];
+            comboBox2.SelectedItem = comboBox2.Items[1];
+            comboBox3.SelectedItem = comboBox3.Items[2];
+            textBox1.Text = "" + RTP_P;
+            textBox2.Text = "" + RTP_S;
+            textBox3.Text = "" + RTC_P;
+            textBox4.Text = "" + RTC_S;
+            RT();
+        }
+        public double F2_RTP = 0;
+        public double F2_RTC = 0;
+        void RT()
+        {
+            F2_RTP = Convert.ToDouble(textBox1.Text.ToString()) / Convert.ToDouble(textBox2.Text.ToString());
+            F2_RTC = Convert.ToDouble(textBox3.Text.ToString()) / Convert.ToDouble(textBox4.Text.ToString());
         }
         void draw_design()
         {
@@ -39,6 +75,53 @@ namespace Prot_Sistemas
             finally
             {
             }
+        }
+        public string[] Analogico_A_forms2;
+        public string[] Analogico_V_forms2;
+        public string Tensao_VA;
+        public string Tensao_VB;
+        public string Tensao_VC;
+        public string Corrente_IA;
+        public string Corrente_IB;
+        public string Corrente_IC;
+        public string RTP_P;
+        public string RTP_S;
+        public string RTC_P;
+        public string RTC_S;
+        public string ZL1_re;
+        public string ZL1_im;
+        public string ZL0_re;
+        public string ZL0_im;
+        public string Lengh_Line;
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Tensao_VA = comboBox1.Text.ToString();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Tensao_VB = comboBox2.Text.ToString();
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Tensao_VC = comboBox3.Text.ToString();
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Corrente_IA = comboBox4.Text.ToString();
+        }
+
+        private void comboBox5_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Corrente_IB = comboBox5.Text.ToString();
+        }
+
+        private void comboBox6_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Corrente_IC = comboBox6.Text.ToString();
         }
     }
 }
